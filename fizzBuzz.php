0d967557611d256e9a3e96445538b3f3d7a3cebc
srinibharath <srini.bharath@gmail.com>
2015-07-26 13:42:02 +0530

    Create fizzBuzz.php

---
+++ fizzBuzz.php
@@ -0,0 +1,19 @@
+    function FizzBuzz() {
+        // disable auto rendering
+        $this->autoRender = false;
+
+        // loop to iterate through +ve integers 1-100
+        for($i=1; $i<=100 ; $i++) {
+
+            // mutually exclusive conditions to check for divisibility by 15, 3 and 5 in that order.
+            if($i % 15 == 0) {
+                echo "FizzBuzz" . "<br/>";
+            } elseif($i % 3 == 0) {
+                echo "Fizz" . "<br/>";
+            } elseif($i % 5 == 0) {
+                echo "Buzz" . "<br/>";
+            } else {
+                echo $i . "<br/>";
+            }
+        }
+    }
