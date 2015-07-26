c4593c0d3fa642866488aa98ba529200d201227e
srinibharath <srini.bharath@gmail.com>
2015-07-26 13:43:41 +0530

    Create fibonacciSequence.php

---
+++ fibonacciSequence.php
@@ -0,0 +1,15 @@
+    function fibonacciSequence($n) {
+        $this->autoRender = false;
+        $penultimate    = 0;
+        $previous       = 1;
+        
+        echo $penultimate . "\n";
+        echo $previous . "\n";
+        
+        for ($i=1; $i<=$n-2; $i++) {
+            $current = $penultimate + $previous;
+            $penultimate = $previous;
+            $previous = $current;
+            echo $current . "\n";
+        }
+    }
